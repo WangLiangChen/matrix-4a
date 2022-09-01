e1e333b7f78f96e460b58e657c7bda63e34f2040
Fengyuan.Wang <fengyuan.wang@hotmail.com>
2022-09-01 21:04:01 +0800

    重构IAM

--- matrix-iam-authorization/src/main/java/wang/liangchen/matrix/iam/authorization/domain/allowlist/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author Liangchen.Wang 2022-07-07 11:58
- */
-@Aggregate(name = "allowlist")
-package wang.liangchen.matrix.iam.authorization.domain.allowlist;
-
-import wang.liangchen.matrix.framework.ddd.domain.Aggregate;
--- matrix-iam-authorization/src/main/java/wang/liangchen/matrix/iam/authorization/southbound_acl/port/repository/AuthorizationSubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package wang.liangchen.matrix.iam.authorization.southbound_acl.port.repository;
-
-/**
- * @author Liangchen.Wang 2022-08-17 11:02
- */
-public interface AuthorizationSubjectRepository extends CommonRepository{
-}
