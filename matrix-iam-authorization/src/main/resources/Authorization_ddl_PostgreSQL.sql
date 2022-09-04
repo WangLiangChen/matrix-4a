82a77895aa1bb24007a39b0368472b78e4ce4fa3
Fengyuan.Wang <fengyuan.wang@hotmail.com>
2022-09-04 22:30:18 +0800

    new Criteria

--- matrix-iam-authorization/src/main/resources/Authorization_ddl_PostgreSQL.sql
+++ matrix-iam-authorization/src/main/resources/Authorization_ddl_PostgreSQL.sql
@@ -1,4 +1,72 @@
 /*==============================================================*/
+/* Table: authorization_consumer                                */
+/*==============================================================*/
+create table authorization_consumer (
+    consumer_id          INT8                 not null,
+    tenant_code          VARCHAR(36)          not null,
+    consumer_code        VARCHAR(36)          not null,
+    consumer_name        VARCHAR(36)          not null,
+    grant_version        INT4                 not null,
+    assign_version       INT4                 not null,
+    version              INT4                 not null default 0,
+    owner                VARCHAR(60)          not null default '',
+    creator              VARCHAR(60)          not null default '',
+    create_datetime      timestamp without time zone                 not null,
+    modifier             VARCHAR(60)          not null default '',
+    modify_datetime      timestamp without time zone                 not null,
+    summary              VARCHAR(200)         not null default '',
+    state                VARCHAR(36)          not null,
+    constraint PK_AUTHORIZATION_CONSUMER primary key (consumer_id)
+);
+
+comment on table authorization_consumer is
+'消费端';
+
+comment on column authorization_consumer.consumer_id is
+'Primarykey';
+
+comment on column authorization_consumer.consumer_name is
+'名称';
+
+comment on column authorization_consumer.grant_version is
+'该consumer的subject和role关系变动,值累加';
+
+comment on column authorization_consumer.assign_version is
+'该consumer的role_resource_operation关系变动,值累加';
+
+comment on column authorization_consumer.version is
+'版本号,可用于乐观锁';
+
+comment on column authorization_consumer.owner is
+'属主标识';
+
+comment on column authorization_consumer.creator is
+'创建者标识';
+
+comment on column authorization_consumer.create_datetime is
+'创建时间,长度6';
+
+comment on column authorization_consumer.modifier is
+'最后修改者标识';
+
+comment on column authorization_consumer.modify_datetime is
+'最后修改时间,长度6';
+
+comment on column authorization_consumer.summary is
+'简述说明';
+
+comment on column authorization_consumer.state is
+'状态';
+
+/*==============================================================*/
+/* Index: authorization_consumer_PK                             */
+/*==============================================================*/
+create unique index authorization_consumer_PK on authorization_consumer (
+    consumer_id
+);
+
+
+/*==============================================================*/
 /* Table: authorization_subject                                 */
 /*==============================================================*/
 create table authorization_subject (
@@ -129,7 +197,7 @@ group_id
 );
 
 /*==============================================================*/
-/* Index: group_self_FK                                         */
+/* Index: group_self_FK     */
 /*==============================================================*/
 create  index group_self_FK on authorization_group (
 parent_id
@@ -721,14 +789,14 @@ comment on column authorization_role_policy.state is
 '状态';
 
 /*==============================================================*/
-/* Index: role_policy_FK                                        */
+/* Index: role_policy_FK    */
 /*==============================================================*/
 create  index role_policy_FK on authorization_role_policy (
 role_id
 );
 
 /*==============================================================*/
-/* Index: policy_role_FK                                        */
+/* Index: policy_role_FK    */
 /*==============================================================*/
 create  index policy_role_FK on authorization_role_policy (
 policy_id
